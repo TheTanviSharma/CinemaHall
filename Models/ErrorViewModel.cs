307d8f2b07d495242aadea46ec2278f0beba63f8
www <www@DESKTOP-NEG7C23>
2023-07-06 06:23:40 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CinemaHall.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
